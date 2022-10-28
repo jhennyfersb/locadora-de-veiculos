9ffcc6071935fa4a7b7b596a79e7ec4cf8ff368a
Bruno Pelliciari <bruno.pelliciari@hotmail.com>
2022-10-27 21:17:49 -0300

    brabo2

--- src/test/java/br/com/dbc/vemser/sistemaaluguelveiculos/SistemaAluguelVeiculosApplicationTests.java
+++ src/test/java/br/com/dbc/vemser/sistemaaluguelveiculos/SistemaAluguelVeiculosApplicationTests.java
@@ -1,13 +1,13 @@
-package br.com.dbc.vemser.sistemaaluguelveiculos;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class SistemaAluguelVeiculosApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package br.com.dbc.vemser.sistemaaluguelveiculos;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class SistemaAluguelVeiculosApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
